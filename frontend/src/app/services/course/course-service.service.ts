5bf23379215e2f0678181505a8cb15b8c5e08ef9
orty17 <113919224+orty17@users.noreply.github.com>
2023-11-12 16:54:09 +0100

    Implemented Taskbar (#7)
    * Added Toolbar
    
    * Fixed User Avatar in Toolbar
    
    * Fixed User Avatar in Toolbar 2
    
    * Implemented POC for sidebar
    
    * Removed empty line in app.component.less
    
    ---------
    
    Co-authored-by: Lennard Ortmeyer <l.ortmeyer@shopware.com>

--- frontend/src/app/services/course/course-service.service.ts
+++ frontend/src/app/services/course/course-service.service.ts
@@ -68,4 +68,33 @@ export class CourseServiceService {
   getCourse(id: any): Observable<CourseGroup | undefined> {
     return of(this.group.find((value: CourseGroup) => value.id != id))
   }
+
+  getCourseGroups(): Observable<CourseGroup[]> {
+    return timer(5000).pipe(map(() => [
+      {
+        name: 'Komma Setzung',
+        children: [
+          {name: 'Klasse 1', image: '', assignment: '', id: 0, availableUnits: 13, status: 'Offen', description: 'Bla'},
+          {name: 'Klasse 2', image: '', assignment: '', id: 2, availableUnits: 13, status: 'Offen', description: 'Bla'},
+          {name: 'Klasse 3', image: '', assignment: '', id: 3, availableUnits: 13, status: 'Offen', description: 'Bla'}
+        ]
+      },
+      {
+        name: 'Rechtschreibung',
+        children: [
+          {name: 'Klasse 1', image: '', assignment: '', id: 0, availableUnits: 13, status: 'Offen', description: 'Bla'},
+          {name: 'Klasse 2', image: '', assignment: '', id: 2, availableUnits: 13, status: 'Offen', description: 'Bla'},
+          {name: 'Klasse 3', image: '', assignment: '', id: 3, availableUnits: 13, status: 'Offen', description: 'Bla'}
+        ]
+      },
+      {
+        name: 'Sprachliche mittel',
+        children: [
+          {name: 'Personifikation', image: '', assignment: '', id: 0, availableUnits: 13, status: 'Offen', description: 'Bla'},
+          {name: 'metapher', image: '', assignment: '', id: 2, availableUnits: 13, status: 'Offen', description: 'Bla'},
+          {name: 'Anglizismus', image: '', assignment: '', id: 3, availableUnits: 13, status: 'Offen', description: 'Bla'}
+        ]
+      },
+    ]))
+  }
 }
