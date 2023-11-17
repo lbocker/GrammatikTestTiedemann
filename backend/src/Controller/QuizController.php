b7664835d3e2cee439c6822ad458218353685fce
hasanmohsen <hasanmohsen1@icloud.com>
2023-11-17 17:02:35 +0100

    Add UserEntity + create QuizEntity & Controller with API endpoints

---
+++ backend/src/Controller/QuizController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Quiz;
+
+#[Route('/api', name: 'api_')]
+class QuizController extends AbstractController
+{
+    #[Route('/quiz', name: 'quiz_index', methods:['get'] )]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        $quizManager = $doctrine
+            ->getRepository(Quiz::class)
+            ->findAll();
+
+        $data = [];
+
+        foreach ($quizManager as $quiz) {
+            $data[] = [
+                'id' => $quiz->getId(),
+                'title' => $quiz->getTitle(),
+                'description' => $quiz->getDescription(),
+                'category' => $quiz->getCategory(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+
+    #[Route('/quiz', name: 'quiz_create', methods:['post'] )]
+    public function create(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+
+        $quiz = new Quiz();
+        $quiz->setTitle($request->request->get('title'));
+        $quiz->setDescription($request->request->get('description'));
+        $quiz->setCategory($request->request->get('category'));
+
+        $entityManager->persist($quiz);
+        $entityManager->flush();
+
+        $data =  [
+            'id' => $quiz->getId(),
+            'title' => $quiz->getTitle(),
+            'description' => $quiz->getDescription(),
+            'category' => $quiz->getCategory(),
+        ];
+
+        return $this->json($data);
+    }
+
+
+    #[Route('/quiz/{id}', name: 'quiz_show', methods:['get'] )]
+    public function show(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $quiz = $doctrine->getRepository(Quiz::class)->find($id);
+
+        if (!$quiz) {
+
+            return $this->json('No quiz found for id ' . $id, 404);
+        }
+
+        $data =  [
+            'id' => $quiz->getId(),
+            'title' => $quiz->getTitle(),
+            'description' => $quiz->getDescription(),
+            'category' => $quiz->getCategory(),
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('/quiz/{id}', name: 'quiz_update', methods:['put', 'patch'] )]
+    public function update(ManagerRegistry $doctrine, Request $request, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $quiz = $entityManager->getRepository(Quiz::class)->find($id);
+
+        if (!$quiz) {
+            return $this->json('No quiz found for id ' . $id, 404);
+        }
+
+        $quiz->setTitle($request->request->get('title'));
+        $quiz->setDescription($request->request->get('description'));
+        $entityManager->flush();
+
+        $data =  [
+            'id' => $quiz->getId(),
+            'title' => $quiz->getTitle(),
+            'description' => $quiz->getDescription(),
+            'category' => $quiz->getCategory(),
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('/quiz/{id}', name: 'quiz_delete', methods:['delete'] )]
+    public function delete(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $quiz = $entityManager->getRepository(Quiz::class)->find($id);
+
+        if (!$quiz) {
+            return $this->json('No quiz found for id' . $id, 404);
+        }
+
+        $entityManager->remove($quiz);
+        $entityManager->flush();
+
+        return $this->json('Deleted a quiz successfully with id ' . $id);
+    }
+}
+
