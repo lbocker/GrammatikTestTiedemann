c6febdf2c8103c30a5d119ed75dffed5fbe557de
hasanmohsen1 <h.mohsen@shopmacher.de>
2023-11-25 16:06:03 +0100

    Add User API logic with JWT Token & update Docker for local development

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
