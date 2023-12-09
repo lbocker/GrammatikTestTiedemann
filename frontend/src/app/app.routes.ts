91fd4d40bb9d0d758f49484b1e11af82c2790037
Lennard Ortmeyer <l.ortmeyer@shopware.com>
2023-12-09 18:54:18 +0100

    Implement Login Logic

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import {CourseOverviewComponent} from './components/course-overview/course-overview.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 export const routes: Routes = [
   { path: '', component: CardComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
   { path: 'course/:courseId', children: [
       {path: 'overview', component: CourseOverviewComponent}
     ]}
