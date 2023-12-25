7b50af4c6d2644ddb92c18df9e8c76448183764b
lbocker <l.boecker@shopware.com>
2023-12-25 23:41:46 +0100

    Frontend - Fixing Eslint

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PRIMENG_BARREL } from "../../barrel/primeng.barrel";
-import { RippleModule } from "primeng/ripple";
+import { PRIMENG_BARREL } from '../../barrel/primeng.barrel';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-login',
