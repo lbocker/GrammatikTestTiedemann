1d3bb103d84e166842085d7d7f700a5cea9bd425
Lukas Böcker <81483010+lbocker@users.noreply.github.com>
2024-03-15 10:26:22 +0100

    Backend - Change Fields (#59)

--- backend/src/Controller/Admin/CourseCrudController.php
+++ backend/src/Controller/Admin/CourseCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Course;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CourseCrudController extends AbstractCrudController
@@ -17,7 +18,7 @@ class CourseCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield TextField::new('title');
-        yield TextField::new('description');
+        yield TextareaField::new('description');
         yield CollectionField::new('quizSets')->useEntryCrudForm(
             QuizSetsCrudController::class, 'new_quiz_sets_page', 'edit_quiz_sets_page',
         );
