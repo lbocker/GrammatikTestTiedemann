13103127d018caf17500ff3987b56647c4281d4a
lbocker <l.boecker@shopware.com>
2024-03-15 08:58:54 +0100

    Backend - Change Fields

--- backend/src/Controller/Admin/QuizCrudController.php
+++ backend/src/Controller/Admin/QuizCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Quiz;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class QuizCrudController extends AbstractCrudController
@@ -16,7 +17,7 @@ class QuizCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield TextField::new('type');
+        yield ChoiceField::new('type')->autocomplete();
         yield TextField::new('question');
         yield ArrayField::new('rightAnswer');
         yield ArrayField::new('wrongAnswer');
