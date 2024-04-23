ed46f505a3927948253272547e9c160f29262e3d
Lukas Böcker <81483010+lbocker@users.noreply.github.com>
2024-04-23 21:25:34 +0200

    Backend - Fix Backend (#69)

--- backend/migrations/Version20240414130727.php
+++ backend/migrations/Version20240414130727.php
@@ -20,10 +20,10 @@ final class Version20240414130727 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user_courses_status (id INT AUTO_INCREMENT NOT NULL, status VARCHAR(255) NOT NULL, courses_id INT DEFAULT NULL, user_id INT DEFAULT NULL, INDEX IDX_4FAC2F335359E06E (courses_id), INDEX IDX_4FAC2F339D86650F (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE user_courses_status (id INT AUTO_INCREMENT NOT NULL, status VARCHAR(255) NOT NULL, courses_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_4FAC2F335359E06E (courses_id), INDEX IDX_4FAC2F339D86650F (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE user_courses_status ADD CONSTRAINT FK_4FAC2F335359E06E FOREIGN KEY (courses_id) REFERENCES courses (id)');
         $this->addSql('ALTER TABLE user_courses_status ADD CONSTRAINT FK_4FAC2F339D86650F FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE quiz ADD points INT DEFAULT 0 NOT NULL, CHANGE question question VARCHAR(255) NOT NULL, CHANGE right_answer right_answer JSON NOT NULL, CHANGE wrong_answer wrong_answer JSON NOT NULL, CHANGE type type VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE quiz ADD points INT DEFAULT 0 NOT NULL, CHANGE question question VARCHAR(255) NOT NULL, CHANGE right_answer right_answer TEXT NOT NULL, CHANGE wrong_answer wrong_answer TEXT NOT NULL, CHANGE type type VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE quiz_sets CHANGE title title VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE user CHANGE points points INT DEFAULT 0 NOT NULL');
     }
